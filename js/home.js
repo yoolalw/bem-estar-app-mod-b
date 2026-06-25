7cba984b2b37e2e9de57e2e358018472e69bd889
yoolalw <larakemilyprofissional@gmail.com>
2026-06-25 13:11:45 -0300

    Trocando url de lugar

--- js/home.js
+++ js/home.js
@@ -1,10 +1,10 @@
 console.log("a")
 const token = localStorage.getItem('token')
 console.log(token)
-const urlToFetch = "https://workspace.dinizeotecnologia.com.br/seletiva_pr_a2/desafio/1"
 async function ativs() {
     try {
-        const response = await fetch(urlToFetch, {
+        const response = await fetch("https://workspace.dinizeotecnologia.com.br/seletiva_pr_a2/desafio/2", {
+            method: 'GET',
             headers: {
                 "Authorization": `Bearer ${token}`
             }
@@ -23,10 +23,10 @@ async function ativs() {
     }
 } ativs()
 
-function sobre(){
+function sobre() {
     window.location.href = "sobre.html"
 }
 
-function desafios(){ 
+function desafios() {
     window.location.href = "desafios.html"
 }
